9e56ddec584994f854e8c6628dac133e07c0aabb
Johnny <evgkisevgkis@gmail.com>
2023-10-16 19:49:27 +0500

    Создание класса для получения вакансий с HH

--- src/apis.py
+++ src/apis.py
@@ -1,16 +1,27 @@
 import requests
 from abc import ABC, abstractmethod
+import json
 
 
 class API(ABC):
     """Абстрактный класс для получения данных через API"""
     def __init__(self):
         self.vacancies = None
-        self.query_param = None
-        self.salary = None
-        self.url = None
-        self.req = requests.get(self.url, self.query_param)
 
     @abstractmethod
     def request_vacancies(self):
         pass
+
+
+class HeadHunterAPI(API):
+    """Класс для получения данных с HH API"""
+    def request_vacancies(self, page=0):
+        query_param = {'text': 'NAME:Python',
+                       'page': page,
+                       'per_page': 100}
+        req = requests.get('https://api.hh.ru/vacancies', query_param)
+        return req.json()
+
+
+hh = HeadHunterAPI()
+print(json.dumps(hh.request_vacancies(), ensure_ascii=False, indent=1))
