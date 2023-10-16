3e4f2a2b77ee2af427eef660105c1d24d0e7e075
Johnny <evgkisevgkis@gmail.com>
2023-10-16 22:32:43 +0500

    Добавление класса для получения вакансий с Super Job

--- src/apis.py
+++ src/apis.py
@@ -27,3 +27,20 @@ class HeadHunterAPI(API):
                 self.vacancies.append(i)
             page += 1
         return self.vacancies
+
+
+class SuperJobAPI(API):
+    """Класс для получения данных с SuperJob API"""
+    def request_vacancies(self, page=0):
+        header = {'X-Api-App-Id':
+                  'v3.r.137892971.9bd1a19d86b81cd78d694bcd9df19f7c3ace390f.d74685e56bddcdaac706bb573ee629b35d86f2b0'}
+        query_param = {'keyword': 'Python', 'page': page, 'count': 100}
+        req = requests.get('https://api.superjob.ru/2.0/vacancies', headers=header, params=query_param)
+        pages_count = round(req.json()['total'] / 100)
+        while page <= pages_count:
+            req = requests.get('https://api.superjob.ru/2.0/vacancies', headers=header, params=query_param)
+            vacancies = req.json()['objects']
+            for i in vacancies:
+                self.vacancies.append(i)
+            page += 1
+        return self.vacancies
