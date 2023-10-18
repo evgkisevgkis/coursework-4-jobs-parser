fa74bc26b0fc1124169572b90355020843bb427a
Johnny <evgkisevgkis@gmail.com>
2023-10-18 19:54:22 +0500

    Добавлена функция добавления данных в конец файла

--- src/files.py
+++ src/files.py
@@ -11,3 +11,8 @@ class FileWorker:
         with open(self.path, 'w', encoding='utf-8') as f:
             for i in data:
                 f.write(json.dumps(i, ensure_ascii=False, indent=1))
+
+    def add_to_json(self, data):
+        """Функция добавляет в конец файла переданные в неё данные"""
+        with open(self.path, 'a', encoding='utf-8') as f:
+            f.write(json.dumps(data, ensure_ascii=False, indent=1))
